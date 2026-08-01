cb56d9c01a5788e60e3c720abf9f153860f858e5
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-31 21:54:55 -0500

    Remove cpp prefix

--- include/collections/circular_list.hpp
+++ include/collections/circular_list.hpp
@@ -1,4 +1,16 @@
-#ifndef CPP_CIRCULAR_LIST_HPP
-#define CPP_CIRCULAR_LIST_HPP
+#ifndef COLLECTIONS_CIRCULAR_LIST_HPP
+#define COLLECTIONS_CIRCULAR_LIST_HPP
 
-#endif // #ifndef CPP_CIRCULAR_LIST_HPP
+// ISO C Includes
+#include <cstddef>
+
+// ISO C++ Includes
+#include <memory>
+
+namespace collections {
+    template<typename T, typename Allocator = std::allocator<T>>
+    class circular_list {
+    };
+} // namespace collections
+
+#endif // #ifndef COLLECTIONS_CIRCULAR_LIST_HPP
